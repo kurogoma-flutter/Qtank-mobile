1f695cbc860b8cc5dc69c9aca2ff747d6a867380
Kurogoma939 <kan.sss.939@gmail.com>
2022-07-17 00:04:13 +0900

    設定ファイル追加

--- lib/presentation/pages/workspace/workspace_setting_page.dart
+++ lib/presentation/pages/workspace/workspace_setting_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:qtank_mobile/presentation/style/color.dart';
 
+import '../../../data/utility/logger/logger.dart';
+
 class WorkSpaceSettingPage extends StatelessWidget {
   const WorkSpaceSettingPage({Key? key}) : super(key: key);
 
@@ -17,7 +19,29 @@ class WorkSpaceSettingPage extends StatelessWidget {
         child: SizedBox(
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height,
-          child: const Center(child: Text('ワークスペース設定')),
+          child: Column(
+            children: <Widget>[
+              GestureDetector(
+                onTap: () {
+                  logger.i('image picker');
+                },
+                child: SizedBox(
+                  width: 80,
+                  height: 80,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.asset(
+                      'assets/tank-solo.png',
+                      fit: BoxFit.cover,
+                      errorBuilder: (context, error, stackTrace) {
+                        return const Icon(Icons.error);
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
