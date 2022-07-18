19ab749985064fdb5fee4709da7deac3daf291e3
Kurogoma939 <kan.sss.939@gmail.com>
2022-07-18 20:04:52 +0900

    プロフィールをFirestoreから取得

--- lib/presentation/pages/user/user_profile_page.dart
+++ lib/presentation/pages/user/user_profile_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:qtank_mobile/data/utility/logger/logger.dart';
+import 'package:go_router/go_router.dart';
 import 'package:qtank_mobile/presentation/style/color.dart';
 
+import '../../../data/utility/logger/logger.dart';
 import '../../../data/view_model/auth_page_view_model.dart';
+import '../../../data/view_model/user_page_view_model.dart';
 import '../../style/style.dart';
 import '../common_components/dialog.dart';
 
@@ -13,6 +15,7 @@ class UserProfilePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final viewModel = ref.watch(authPageProvider);
+    final future = ref.watch(usersFutureProvider);
     return Scaffold(
       backgroundColor: QTankColor.black,
       appBar: AppBar(
@@ -21,82 +24,98 @@ class UserProfilePage extends ConsumerWidget {
         elevation: 0,
         centerTitle: false,
       ),
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const CircleAvatar(
-              radius: 48,
-              backgroundImage: AssetImage('assets/dammy_icon_3.png'),
-            ),
-            const Text(
-              'ユーザー名',
-              style: QTankTextStyle.largeTitle,
-            ),
-            const _TitleLabelWithBoldText(label: 'ID', text: '@VdPA53GW4g6BW'),
-            const _TitleLabelWithBoldText(
-                label: 'Email', text: 'dammy@example.com'),
-            const _TitleLabelWithBoldText(label: '所属', text: 'ソリューション事業部 第二課'),
-            const _TitleLabelWithBoldText(label: '役職', text: '人事部長補佐'),
-            const SizedBox(),
-            const _TitleLabelWithText(label: '最終更新', text: '2022/02/02'),
-            ElevatedButton(
-              onPressed: () {
-                logger.i('プロフィール編集');
-              },
-              style: ElevatedButton.styleFrom(
-                primary: QTankColor.orange,
-                onPrimary: QTankColor.white,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
+      body: future.when(
+        data: (user) {
+          if (!user.exists) {
+            context.go('/auth/login');
+          }
+
+          final userData = user.data();
+          return SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const CircleAvatar(
+                  radius: 48,
+                  backgroundImage: AssetImage('assets/dammy_icon_3.png'),
                 ),
-              ),
-              child: const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
-                child: Text(
-                  'プロフィール編集',
-                  style: QTankTextStyle.buttonText,
+                Text(
+                  userData!['name'] ?? '未設定',
+                  style: QTankTextStyle.largeTitle,
                 ),
-              ),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Icon(
-                    Icons.logout_rounded,
-                    color: QTankColor.red,
-                  ),
-                  TextButton(
-                    onPressed: () async {
-                      var dialogResult = await showDialog<bool>(
-                        context: context,
-                        builder: (_) {
-                          return const ConfirmationDialog(
-                            dialogMessage: 'ログアウトしてもよろしいですか？',
-                          );
-                        },
-                      );
-                      if (dialogResult == true) {
-                        logger.i('サインアウト処理');
-                        // ignore: use_build_context_synchronously
-                        viewModel.signOut(context);
-                      }
-                    },
-                    child: const Text(
-                      'アプリからサインアウト',
-                      style: QTankTextStyle.alertTextBold,
+                _TitleLabelWithBoldText(
+                    label: 'ID', text: '@${userData["uid"] ?? "未設定"}'),
+                _TitleLabelWithBoldText(
+                    label: 'Email', text: '${userData["email"] ?? "未設定"}'),
+                _TitleLabelWithBoldText(
+                    label: '所属', text: '${userData["belong"] ?? "未設定"}'),
+                _TitleLabelWithBoldText(
+                    label: '役職', text: '${userData["position"] ?? "未設定"}'),
+                const SizedBox(),
+                _TitleLabelWithText(
+                    label: '最終更新',
+                    text: userData["updatedAt"].toDate().toString()),
+                ElevatedButton(
+                  onPressed: () {
+                    logger.i('プロフィール編集');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: QTankColor.orange,
+                    onPrimary: QTankColor.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
                     ),
                   ),
-                ],
-              ),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+                    child: Text(
+                      'プロフィール編集',
+                      style: QTankTextStyle.buttonText,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.logout_rounded,
+                        color: QTankColor.red,
+                      ),
+                      TextButton(
+                        onPressed: () async {
+                          var dialogResult = await showDialog<bool>(
+                            context: context,
+                            builder: (_) {
+                              return const ConfirmationDialog(
+                                dialogMessage: 'ログアウトしてもよろしいですか？',
+                              );
+                            },
+                          );
+                          if (dialogResult == true) {
+                            logger.i('サインアウト処理');
+                            // ignore: use_build_context_synchronously
+                            viewModel.signOut(context);
+                          }
+                        },
+                        child: const Text(
+                          'アプリからサインアウト',
+                          style: QTankTextStyle.alertTextBold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          );
+        },
+        error: (err, stack) => Text('Error: $err'),
+        loading: () => const Center(child: CircularProgressIndicator()),
       ),
     );
   }
