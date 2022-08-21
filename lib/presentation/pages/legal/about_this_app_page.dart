ddc013f1f8d65d7a11348bd59bc6ca869caa4b7e
Kurogoma939 <kan.sss.939@gmail.com>
2022-08-21 23:37:24 +0900

    このアプリについて画面にテキスト追加

--- lib/presentation/pages/legal/about_this_app_page.dart
+++ lib/presentation/pages/legal/about_this_app_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:qtank_mobile/presentation/pages/legal/string/about_this_app.dart';
+import 'package:qtank_mobile/presentation/style/style.dart';
 
 import '../../style/color.dart';
 
@@ -15,10 +17,51 @@ class AboutThisAppPage extends StatelessWidget {
         centerTitle: true,
         elevation: 0,
       ),
-      body: const SizedBox.expand(
-        child: Center(
-          child: Text('このアプリについて'),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20, top: 40),
+        child: SizedBox.expand(
+          child: Column(
+            children: const [
+              _AppInfoLabel(title: 'アプリバージョン', content: appVersion),
+              _AppInfoLabel(title: '製作者', content: maker),
+              _AppInfoLabel(title: '問合せ先', content: inquiryEmail),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class _AppInfoLabel extends StatelessWidget {
+  const _AppInfoLabel({
+    Key? key,
+    required this.title,
+    required this.content,
+  }) : super(key: key);
+
+  final String title;
+  final String content;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        children: [
+          Text(
+            title,
+            style: QTankTextStyle.miniTitle,
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            child: Text(':', style: QTankTextStyle.miniTitle),
+          ),
+          Text(
+            content,
+            style: QTankTextStyle.subtitle,
+          ),
+        ],
       ),
     );
   }
---
+++ lib/presentation/pages/legal/string/about_this_app.dart
@@ -0,0 +1,3 @@
+const String appVersion = '0.5.0';
+const String maker = 'くろごま';
+const String inquiryEmail = 'sample.dummy@gmail.com';
